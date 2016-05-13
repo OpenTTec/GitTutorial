9ad40200a9ebc1b57f4ccc38c7dd66681240766e
IrfaanKamo <irfaan.kamo@gmail.com>
2016-05-13 17:40:14 +0200

    Adding User IrfaanK (#10)

---
+++ CodeMonkeys/IrfaanK.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace GitTutorial.CodeMonkeys
+{
+  class IrfaanK : CodeMonkey
+  {
+    //-------------------------------------------------------------------------
+
+    protected override string GetName()
+    {
+      return "IrfaanK";
+    }
+
+    //-------------------------------------------------------------------------
+
+    public override Color GetFavouriteColour()
+    {
+      return Color.Indigo;
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}

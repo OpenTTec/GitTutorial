ac71fa8b4bacaadb0fd2c36019cf95d39ef6b7c6
GraemeB <grae22@gmail.com>
2016-05-13 14:39:02 +0200

    Adding TestUser.

---
+++ CodeMonkeys/TestUser.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace GitTutorial.CodeMonkeys
+{
+  class TestUser : CodeMonkey
+  {
+    //-------------------------------------------------------------------------
+
+    protected override string GetName()
+    {
+      return "TestUser";
+    }
+
+    //-------------------------------------------------------------------------
+
+    public override Color GetFavouriteColour()
+    {
+      return Color.Red;
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}

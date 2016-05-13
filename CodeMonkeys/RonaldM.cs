a19475fd9e0a983986da25f66343c9bdfe470a97
Ronald Mariah <ronaldmariah@gmail.com>
2016-05-13 15:14:27 +0200

    Adding User RonaldM

---
+++ CodeMonkeys/RonaldM.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace GitTutorial.CodeMonkeys
+{
+  internal class RonaldM : CodeMonkey
+  {
+    //-------------------------------------------------------------------------
+
+    protected override string GetName()
+    {
+      return "RonaldM";
+    }
+
+    //-------------------------------------------------------------------------
+
+    public override Color GetFavouriteColour()
+    {
+      return Color.BlanchedAlmond;
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
